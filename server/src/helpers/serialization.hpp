d2bceadd30cdd691acd80e4fe93f2befabb77cd4
scorsi <sylvain.corsini@gmail.com>
2018-11-25 14:51:36 +0100

    Adding template specialization for string

--- server/src/helpers/serialization.hpp
+++ server/src/helpers/serialization.hpp
@@ -2,6 +2,7 @@
 
 #include <sstream>
 #include <cereal/archives/binary.hpp>
+#include "serialize/string.hpp"
 
 namespace helpers {
 namespace serialization {
@@ -12,10 +13,22 @@ void load(T &data, std::stringstream &payload) {
     archive(data);
 }
 
+template<>
+void load(std::string &data, std::stringstream &payload) {
+    cereal::BinaryInputArchive archive(payload);
+    serialize::load_string(archive, data);
+}
+
 template<typename T>
 void save(const T &data, std::stringstream &payload) {
     cereal::BinaryOutputArchive archive(payload);
     archive(data);
+}
+
+template<>
+void save(const std::string &data, std::stringstream &payload) {
+    cereal::BinaryInputArchive archive(payload);
+    serialize::save_string(archive, data);
 }
 
 }
